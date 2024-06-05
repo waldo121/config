6c61f3333df465c654a69955d352dc47c6961912
waldo121 <17034517+waldo121@users.noreply.github.com>
2024-06-05 11:59:09 -0400

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,121 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      <nixos-hardware/lenovo/thinkpad/e14/amd> # single machine managed by nix so far
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.binfmt.registrations.appimage = {
+   wrapInterpreterInShell = false;
+   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+   recognitionType = "magic";
+   offset = 0;
+   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+   magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+  
+  # System upgrades
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;  
+
+  networking.hostName = "nixos"; # Define your hostname.
+  
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+  # Fonts
+  fonts.enableDefaultPackages = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Sway autologin
+  hardware.opengl.enable = true;
+  services.greetd.enable = true;
+  services.greetd.settings = {
+    default_session = {
+      command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+      user = "raphael";
+   };
+  };
+  services.greetd.vt = 1;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.raphael = {
+    isNormalUser = true;
+    description = "raphael";
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    packages = with pkgs; [];
+  };
+
+  # Enable automatic login for the user.
+  # services.getty.autologinUser = "raphael";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = false;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  programs.light.enable = true;
+  # Sound
+  sound.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;    ## If compatibility with 32-bit applications is desired.
+    extraConfig = "load-module module-combine-sink";
+  };
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Enable polkit for sway
+  security.polkit.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ home.nix
@@ -0,0 +1,100 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "raphael";
+  home.homeDirectory = "/home/raphael";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    pass-wayland
+    gnupg
+    grim
+    slurp
+    wl-clipboard
+    mako
+    libnotify
+    xdg-utils # allows opening links in browser
+    (appimageTools.wrapType2 { # Game developped for windows
+      name = "Ankama-Launcher";
+      src = fetchurl {
+        url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
+        sha256 = "sha256-IeV4uo4HwX4InBjM352Jdrrh66dHIFaNkOq8VvXsi9E=";
+      };
+      extraPkgs = pkgs: with pkgs; [
+        wineWowPackages.stable
+        samba
+      ];
+    })
+    discord
+  ];
+  nixpkgs.config.allowUnfreePredicate = _: true;
+  nixpkgs.config.pulseaudio = true;
+  programs.firefox.enable = true;
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      github.vscode-pull-request-github
+    ];
+  };
+  programs.zsh = {
+    enable = true;
+  };
+  programs.git = {
+    enable = true;
+    userName = "waldo121";
+    userEmail = "17034517+waldo121@users.noreply.github.com";
+  };
+  services.mako =  {
+    enable = true;
+    defaultTimeout = 5000;
+    layer = "overlay";
+  };
+  services.gnome-keyring.enable = true;
+  services.gpg-agent.enable = true;
+  services.batsignal.enable = true;
+  
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    wrapperFeatures.base = true;
+    config = rec {
+      output = {
+        eDP-1 = {
+          bg = "~/.config/sway/wallpaper.jpg fill";
+        };
+      };
+      modifier = "Mod4";
+    };
+    extraConfigEarly = "# Brightness
+bindsym XF86MonBrightnessDown exec light -U 10
+bindsym XF86MonBrightnessUp exec light -A 10
+
+# Volume
+bindsym XF86AudioRaiseVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ +5%'
+bindsym XF86AudioLowerVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ -5%'
+bindsym XF86AudioMute exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
+  };
+  home.file.".config/sway/wallpaper.jpg" = {
+    source = resources/bg.jpg;
+  };
+
+  home.sessionVariables = {
+    EDITOR = "codium";
+  };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
