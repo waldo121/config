38d6e06b48f225ac78790e70bfc80dd59d10e13e
waldo121 <17034517+waldo121@users.noreply.github.com>
2024-07-15 11:08:30 -0400

    Update ankama launcher hash and configuration.nix for nixos 24.05

--- configuration.nix
+++ configuration.nix
@@ -14,14 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.binfmt.registrations.appimage = {
-   wrapInterpreterInShell = false;
-   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-   recognitionType = "magic";
-   offset = 0;
-   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-   magicOrExtension = ''\x7fELF....AI\x02'';
-  };
+  programs.appimage.binfmt = true;
   
   # System upgrades
   system.autoUpgrade.enable = true;
--- home.nix
+++ home.nix
@@ -33,7 +33,7 @@
       name = "Ankama-Launcher";
       src = fetchurl {
         url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
-        sha256 = "sha256-7c4SnaCdUpCg9GbpKHLxJAbHPknMWKdEMtHuC7DgxwI=";
+        sha256 = "sha256-XFkx5GOMqaHV55epjp1zbA+gpOlm+yTbiRtRvGDDwMQ=";
       };
       extraPkgs = pkgs: with pkgs; [
         wine64
