a2e816bddf81b38445810277d71aa47565e6dddd
waldo121 <17034517+waldo121@users.noreply.github.com>
2025-06-17 10:57:00 -0400

    remove unused dev tools

--- code.nix
+++ code.nix
@@ -6,11 +6,7 @@
         profiles.default = {
             extensions = with pkgs.vscode-extensions; [
                 bbenoist.nix 
-                ms-azuretools.vscode-docker
-                github.copilot
-                github.copilot-chat
                 visualstudioexptteam.vscodeintellicode
-                golang.go
                 vscodevim.vim
             ];
         };
@@ -18,19 +14,9 @@
         
     };
     home.packages = with pkgs; [
-        go
-        gopls
-        gcc
-        podman-compose
-        podman-tui
-        dbeaver-bin
-        podman
         android-tools
         android-studio-tools
         android-studio
         code-cursor
-        python314
-        ansible
-        gh
     ];
 }
