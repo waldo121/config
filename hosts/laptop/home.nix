5235741c516a9329c725a9f8f143d5b313068f99
waldo121 <17034517+waldo121@users.noreply.github.com>
2026-06-08 12:31:54 -0400

    Migrate from pactl tp wpctl

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -131,9 +131,9 @@
         bindsym XF86MonBrightnessUp exec light -A 10
 
         # Volume
-        bindsym XF86AudioRaiseVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ +5%'
-        bindsym XF86AudioLowerVolume exec 'pactl set-sink-volume @DEFAULT_SINK@ -5%'
-        bindsym XF86AudioMute exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'
+        bindsym XF86AudioRaiseVolume exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+'
+        bindsym XF86AudioLowerVolume exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-'
+        bindsym XF86AudioMute exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'
         bindsym --locked XF86AudioPlay exec playerctl play-pause
         bindsym XF86AudioNext exec playerctl next
         bindsym XF86AudioPrev exec playerctl previous
